13d5436e63ede3b684027079ae9cdd7827948b6f
kadjit228 <114704772+kadjit228@users.noreply.github.com>
2023-02-03 03:44:49 +0300

    new preloader

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
 import Navigation from "./Components/Navigation";
 import About from "./Components/MainPage/sections/About/About";
 import Home from "./Components/MainPage/sections/Home/Home";
@@ -17,6 +17,15 @@ import './reloader.js'
 import Preloader from "./Icons/Preloader";
 
 
+const Title = styled.h1`
+  color: #DD6B20;
+  font-size: 1vw;
+  text-align: center;
+  margin-top: 55vh;
+  @media(max-width: 64em) {
+    font-size: 5vw;
+  }
+`
 function App() {
   return (
     <>
@@ -24,6 +33,9 @@ function App() {
       <ThemeProvider theme={light}>
         <div className="preloader">
           <Preloader />
+          <Title className="title">
+            Loading...
+          </Title>
           <script src="./reloader.js"></script>
         </div>
         <Navigation />
